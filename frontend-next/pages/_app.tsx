8776465a764fd48af67de70ef9ad060129c3f0be
riteshvg <riteshvgupta@gmail.com>
2025-12-10 15:32:41 +0530

    fix: Hide consent banner on marketing simulator page
    - Added route-based conditional rendering in _app.tsx
    - Consent banner now only appears on pages other than /marketing-simulator
    - Marketing simulator page is now consent-free as intended

--- frontend-next/pages/_app.tsx
+++ frontend-next/pages/_app.tsx
@@ -5,6 +5,7 @@ import theme from '../theme/theme';
 import Navbar from '../components/Navbar';
 import { ConsentProvider } from '../lib/consent/ConsentContext';
 import ConsentExperience from '../components/consent/ConsentExperience';
+import { useRouter } from 'next/router';
 
 /**
  * Custom App Component
@@ -14,18 +15,23 @@ import ConsentExperience from '../components/consent/ConsentExperience';
  * - Material-UI ThemeProvider (styling)
  * - CssBaseline (CSS reset)
  * - Navbar (global navigation)
- * - ConsentExperience (consent UI)
+ * - ConsentExperience (consent UI - hidden on marketing simulator page)
  * 
  * Note: Auth0 UserProvider not needed in Pages Router
  * Authentication works via API routes and useUser hook
  */
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    // Hide consent banner on marketing simulator page
+    const showConsent = router.pathname !== '/marketing-simulator';
+
     return (
         <ConsentProvider>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <Navbar />
-                <ConsentExperience />
+                {showConsent && <ConsentExperience />}
                 <Component {...pageProps} />
             </ThemeProvider>
         </ConsentProvider>
